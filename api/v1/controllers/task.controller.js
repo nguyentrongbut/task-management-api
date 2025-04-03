a4631923fd9ae94b93962bf14ca3553afc58119e
nguyentrongbut <cloly031128@gmail.com>
2025-04-04 01:07:30 +0700

    feat: pagination

---
+++ api/v1/controllers/task.controller.js
@@ -0,0 +1,64 @@
+const Task = require("../../../models/task.model");
+
+const paginationHelper = require("../../../helpers/pagination");
+
+// [GET] /api/v1/tasks
+module.exports.index = async (req, res) => {
+    try {
+        const find = {
+            deleted: false
+        }
+
+        if (req.query.status) {
+            find.status = req.query.status;
+        }
+
+        // Pagination
+        let initPagination = {
+            currentPage: 1,
+            limitItems: 2,
+        }
+        const countTasks = await Task.countDocuments(find);
+        const objectPagination = paginationHelper(
+            initPagination,
+            req.query,
+            countTasks
+        )
+        // End Pagination
+
+        // Sort
+        const sort = {}
+
+        if (req.query.sortKey && req.query.sortValue) {
+            sort[req.query.sortKey] = req.query.sortValue;
+        }
+        // End Sort
+
+        const tasks = await Task.find(find)
+                .sort(sort)
+                .limit(objectPagination.limitItems)
+                .skip(objectPagination.skip)
+        ;
+
+        res.json(tasks);
+    } catch (error) {
+        res.json("Error");
+    }
+}
+
+// [GET] /api/v1/tasks/detail/:id
+module.exports.detail = async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const tasks = await Task.findOne({
+            _id: id,
+            deleted: false
+        });
+
+        res.json(tasks);
+    } catch (error) {
+        res.json("Task not found");
+    }
+
+}
