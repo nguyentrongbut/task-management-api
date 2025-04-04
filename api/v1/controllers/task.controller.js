055210ffb7afd214bcbe44c728892d6cc5c117e8
nguyentrongbut <cloly031128@gmail.com>
2025-04-04 14:53:55 +0700

    feat: edit task

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -23,7 +23,7 @@ module.exports.index = async (req, res) => {
     // Pagination
     let initPagination = {
         currentPage: 1,
-        limitItems: 2,
+        limitItems: 3,
     }
 
     // Sort
@@ -133,4 +133,43 @@ module.exports.changeMulti = async (req, res) => {
         });
     }
 
+}
+
+// [POST] /api/v1/tasks/create
+module.exports.create = async (req, res) => {
+    try {
+        const task = new Task(req.body);
+        await task.save();
+
+        res.json({
+            code: 200,
+            message: "Create task success"
+        });
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: "Create task failed",
+        });
+    }
+}
+
+// [PATCH] /api/v1/tasks/edit/:id
+module.exports.edit = async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        await Task.updateOne({
+            _id: id
+        }, req.body);
+
+        res.json({
+            code: 200,
+            message: "Edit task success"
+        });
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: "Edit task failed",
+        });
+    }
 }
